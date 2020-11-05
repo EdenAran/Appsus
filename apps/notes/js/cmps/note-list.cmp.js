cd82829370bea4df0f5c4a9b5e88da780925ffcd
Eden Aran <edenaran@gmail.com>
2020-11-05 17:07:48 +0200

    changed colors

--- apps/notes/js/cmps/note-list.cmp.js
+++ apps/notes/js/cmps/note-list.cmp.js
@@ -6,13 +6,12 @@ import noteEdit from './note-edit.cmp.js';
 export default {
     props: ['notes'],
     template: `
-        <section class="note-list flex just-center wrap" v-if="notes">
-        <!-- <section class="note-list" v-if="notes"> -->
-            <template v-if="havePinned">
-                <note-preview v-for="note in notes" :note="note" :key="note.id" v-if="note.isPinned" @edit="editNote" />
+        <section class="note-list flex just-center wrap al-center">
+            <template v-for="note in notes">
+                <note-preview :note="note" :key="note.id" v-if="note.isPinned" @edit="editNote" />
             </template>
-            <template v-else>
-                <note-preview v-for="note in notes" :note="note" @edit="editNote" :key="note.id" />
+            <template v-for="note in notes">
+                <note-preview :note="note" @edit="editNote" v-if="!note.isPinned" :key="note.id" />
             </template>
             <note-edit v-if="noteToEdit" :note="noteToEdit"  @close="closeEdit"/>
         </section>
