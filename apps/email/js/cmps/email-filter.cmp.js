cd20ea7e7b53382ec434cbf369985ab81e408bd9
Eden Aran <edenaran@gmail.com>
2020-11-06 14:24:43 +0200

    updated menu

--- apps/email/js/cmps/email-filter.cmp.js
+++ apps/email/js/cmps/email-filter.cmp.js
@@ -5,32 +5,41 @@ export default {
     template: `
         <section class="email-filter flex-col">
 
-            <router-link to="/email/compose"><i class="compose fas fa-plus"></i>Compose</router-link>
-
-            <router-link to="/email/inbox"><i class="fas fa-inbox"></i>Inbox<span v-if="numOfInboxUnread"> ({{numOfInboxUnread}})</span></router-link>
-
-            <router-link to="/email/sent"><i class="fas fa-paper-plane"></i>Sent<span v-if="numOfSentUnread"> ({{numOfSentUnread}})</span></router-link>
-
-            <section class="statusRead">
-                <button><i class="fas fa-mail-bulk"></i>Is Read</button>
-                <i class="fas fa-mail-bulk" @click="emitFilter('statusRead', 'all')"></i>
-                <i class="fas fa-envelope-open" @click="emitFilter('statusRead', 'read')"></i>
-                <i class="fas fa-envelope" @click="emitFilter('statusRead', 'unread')"></i>
-            </section>
-            
-            <section class="starred">
-                <button><i class="fas fa-star-half-alt"></i>Is Favorite</button>
-                <i class="fas fa-star-half-alt" @click="emitFilter('starred', 'all')"></i>
-                <i class="fas fa-star" @click="emitFilter('starred', 'favorite')"></i>
-                <i class="far fa-star" @click="emitFilter('starred', 'unfavorite')"></i>
-            </section>
+            <router-link class="comp-btn flex just-center al-center" to="/email/compose"><i class="fas fa-plus"></i> Compose</router-link>
+            <hr>
+            <router-link class="flex" to="/email/inbox"><i class="fas fa-inbox"></i><span>Inbox<span v-if="numOfInboxUnread"> ({{numOfInboxUnread}})</span></span></router-link>
+            <router-link class="flex" to="/email/sent"><i class="fas fa-paper-plane"></i><span>Sent<span v-if="numOfSentUnread"> ({{numOfSentUnread}})</span></span></router-link>
+            <hr>
+            <div class="filter-by">
+                <h4>Filter by:</h4>
+                <section class="statusRead">
+                    <div class="filter-opt flex flex-col" v-if="isFilter.read">
+                        <i title="All" class="option fas fa-mail-bulk" @click="emitFilter('statusRead', 'all')"></i>
+                        <i title="Marked" class="option fas fa-envelope-open" @click="emitFilter('statusRead', 'read')"></i>
+                        <i title="Unmarked" class="option fas fa-envelope" @click="emitFilter('statusRead', 'unread')"></i>
+                    </div> 
+                    <i class="opt-btn fas fa-mail-bulk " @click="toggleOption('read')"><span>  Read</span></i>
+                </section>
+                <section class="starred">
+                    <div class="filter-opt flex flex-col" v-if="isFilter.star">
+                        <i title="All" class="option fas fa-star-half-alt" @click="emitFilter('starred', 'all')"></i>
+                        <i title="Marked" class="option fas fa-star" @click="emitFilter('starred', 'favorite')"></i>
+                        <i title="Unmarked" class="option far fa-star" @click="emitFilter('starred', 'unfavorite')"></i>
+                    </div>
+                    <i class="opt-btn fas fa-star-half-alt" @click="toggleOption('star')"><span>  Favorite</span></i>
+                </section>
+            </div>
 
         </section>
     `,
     data() {
         return {
             numOfInboxUnread: 0,
-            numOfSentUnread: 0
+            numOfSentUnread: 0,
+            isFilter:{
+                star: false,
+                read: false
+            }
         };
     },
     methods: {
@@ -45,7 +54,17 @@ export default {
             }
         },
         emitFilter(key, val) {
+            this.closeOptions()
             this.$emit('filtered', key, val);
+        },
+        toggleOption(opt){
+            if(opt === 'star') this.isFilter.read = false;
+            else this.isFilter.star = false;
+            this.isFilter[opt] = !this.isFilter[opt]
+        },
+        closeOptions(){
+            this.isFilter.read = false;
+            this.isFilter.star = false;
         }
     },
     mounted() {
