1a0aacb6fb3d94825dd1561ca38027dd37020b4b
Shifra Sherrow <yy306090@gmail.com>
2020-11-08 08:10:08 +0200

    new about page

--- js/pages/about.cmp.js
+++ js/pages/about.cmp.js
@@ -2,16 +2,16 @@ export default {
     template: `
         <section class="about-page">
             <div class="main-img">
-                <img src="./assets/img/Untitled-1.png" />
-                <div class="imgs">
-                    <img class="eden" src="./assets/img/Eden.jpg" />
-                    <img class="shifra" src="./assets/img/Shifra.jpg" />
-                </div>
-                <p>
-                    We are two Web developer students that worked our a** off on this site
-                    We really hope you will like it and won't find the bugs... 😜
-                </p>
+                <img src="./assets/img/push-pin.png" />
             </div>
+            <div class="imgs">
+                <img class="eden" src="./assets/img/Eden.jpg" />
+                <img class="shifra" src="./assets/img/Shifra.jpg" />
+            </div>
+            <p>
+                We are two Web developer students that worked our a** off on this site
+                We really hope you will like it and won't find the bugs... 😜
+            </p>
         </section>
     `,
 }
---
+++ js/pages/about2.cmp.js
@@ -0,0 +1,17 @@
+export default {
+    template: `
+        <section class="about-page">
+            <div class="main-img">
+                <img src="./assets/img/Untitled-1.png" />
+                <div class="imgs">
+                    <img class="eden" src="./assets/img/Eden.jpg" />
+                    <img class="shifra" src="./assets/img/Shifra.jpg" />
+                </div>
+                <p>
+                    We are two Web developer students that worked our a** off on this site
+                    We really hope you will like it and won't find the bugs... 😜
+                </p>
+            </div>
+        </section>
+    `,
+}
